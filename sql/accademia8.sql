3ec55d029b7aad3ef8cd7174a183edda08bfe07f
Xeretov <gioeleamendola@gmail.com>
2024-11-19 11:36:57 +0100

    Add accademia8 queries

--- sql/accademia8.sql
+++ sql/accademia8.sql
@@ -1,4 +1,22 @@
 /*
+Definizione dei domini:
+    • Strutturato
+        enum (’Ricercatore’, ’Professore Associato’, ’Professore Ordinario’)
+    • LavoroProgetto
+        enum (’Ricerca e Sviluppo’, ’Dimostrazione’, ’Management’, ’Altro’)
+    • LavoroNonProgettuale
+        enum (’Didattica’, ’Ricerca’, ’Missione’, ’Incontro Dipartimentale’, ’Incontro Accademico’, ’Altro’)
+    • CausaAssenza
+        enum (’Chiusura Universitaria’, ’Maternita’, ’Malattia’)
+    • PosInteger
+        integer ≥ 0
+    • StringaM
+        varchar(100)
+    • NumeroOre
+        integer tra 0 e 8
+    • Denaro
+        real ≥ 0
+
 Persona (id: PosInteger, nome: StringaM, cognome: StringaM, posizione: Strutturato, stipendio: Denaro)
 
 Progetto (id: PosInteger, nome: StringaM, inizio: date, fine: date, budget:Denaro)
@@ -49,3 +67,55 @@ WHERE p.id NOT IN (
         SELECT a.persona
         FROM Assenza a
     );
+
+-- Quali sono le persone (id, nome e cognome) che non hanno mai partecipato ad alcun progetto durante la durata del progetto “Pegasus”?
+SELECT p.id, p.nome, p.cognome
+FROM Persona p
+WHERE p.id NOT IN (
+    SELECT ap.persona
+    FROM AttivitaProgetto ap
+    WHERE ap.progetto IN (
+        SELECT id
+        FROM WP
+        WHERE nome = 'Pegasus'
+        )
+);
+
+-- Quali sono id, nome, cognome e stipendio dei ricercatori 
+-- con stipendio maggiore di tutti i professori (associati e ordinari)?
+SELECT p.id, p.nome, p.cognome, p.stipendio
+FROM Persona p
+WHERE p.id IN (
+    SELECT id
+    FROM Persona
+    WHERE posizione = 'Ricercatore'
+) AND p.stipendio > (
+    SELECT MAX(stipendio)
+    FROM Persona
+    WHERE posizione = 'Professore Associato' OR posizione = 'Professore Ordinario'
+);
+
+-- Quali sono le persone che hanno lavorato su progetti con un budget superiore alla media dei budget di tutti i progetti?
+select distinct p.id, p.nome, p.cognome
+from Persona p
+join AttivitaProgetto ap on p.id = ap.persona
+join WP w on ap.progetto = w.id
+join Progetto pr on w.progetto = pr.id
+where pr.budget > (
+    select avg(budget)
+    from Progetto
+);
+
+-- Quali sono i progetti con un budget inferiore alla media, ma con un numero complessivo di ore dedicate alle attività di ricerca sopra la media?
+select distinct pr.id, pr.nome
+from Progetto pr
+join WP w on pr.id = w.progetto
+join AttivitaProgetto ap on w.id = ap.wp
+where pr.budget < (
+    select avg(budget)
+    from Progetto
+    ) and ap.oreDurata > (
+        select avg(oreDurata)
+        from AttivitaProgetto ap
+        where ap.tipo = 'Ricerca e Sviluppo'
+);
